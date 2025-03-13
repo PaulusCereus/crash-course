3a9848695164b2246723fdee40f0e6ee975adc2a
Pavel Glancev <pavelglancev@gmail.com>
2025-03-13 17:25:03 +0300

    Added get_books_data func to get all data from API

---
+++ requests/requests_books.py
@@ -0,0 +1,30 @@
+import requests
+from prettytable import PrettyTable
+
+def get_books_data(query="", num_results=5):
+    """
+    Функция для получения данных о книгах с использованием Google Books API.
+    Выполняет HTTP-запрос к API и возвращает JSON-ответ с данными о книгах.
+
+    Параметры:
+    query (str): Строка запроса для поиска книг. По умолчанию пустая строка.
+    num_results (int): Количество книг, которое необходимо вернуть. По умолчанию 5.
+
+    Возвращает:
+    dict: Словарь с данными о книгах или сообщение об ошибке.
+    В случае ошибки в запросе, возвращается словарь с ключом 'error' и сообщением об ошибке.
+    """
+    # Формируем URL для запроса с параметрами query и num_results
+    url = f"https://www.googleapis.com/books/v1/volumes?q={query}&maxResults={num_results}"
+
+    try:
+        # Выполняем запрос к API
+        response = requests.get(url)
+        # Проверяем статус ответа (если статус не 200, вызовет исключение)
+        response.raise_for_status()
+        # Возвращаем JSON-ответ в виде словаря
+        return response.json()
+    
+    except requests.exceptions.RequestException as e:
+        # В случае ошибки запроса, выводим сообщение и возвращаем словарь с ошибкой
+        return {"error": f"Ошибка при запросе: {e}"}
