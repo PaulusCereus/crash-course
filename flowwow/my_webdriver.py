fd5b851457b7bd56be24c7c3efe877196d0dc6e4
Pavel Glancev <pavelglancev@gmail.com>
2025-03-17 21:29:36 +0300

    Added _start_driver func to create instance of webdriver

--- flowwow/my_webdriver.py
+++ flowwow/my_webdriver.py
@@ -18,3 +18,26 @@ class MyWebdriver:
         """
         self.headless = headless  # Устанавливаем режим headless (без GUI)
         self.__driver = self._start_driver()  # Запускаем драйвер
+
+    def _start_driver(self):
+        """
+        Настройка и запуск драйвера Chrome с необходимыми параметрами и опциями.
+
+        Возвращает:
+        webdriver.Chrome: Запущенный экземпляр браузера Chrome.
+        """
+        # Загружаем конфигурацию для использования кастомного User-Agent
+        conf = config.MyConfig()
+        options = Options()
+        if self.headless:
+            # Если режим headless, добавляем соответствующий флаг
+            options.add_argument("--headless")
+        # Отключаем некоторые параметры, чтобы избежать ошибок в headless-режиме
+        options.add_argument("--disable-gpu")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument(f"user-agent={conf.get_user_agent()}")  # Добавляем User-Agent из конфигурации
+
+        # Запускаем Chrome с установленными опциями
+        driver = webdriver.Chrome(options=options)
+        return driver
