17b5c73aeedadcdf51190d405738dfb27c5f2d35
Pavel Glancev <pavelglancev@gmail.com>
2025-03-17 21:29:12 +0300

    Added import of modules and empty class

---
+++ flowwow/my_webdriver.py
@@ -0,0 +1,10 @@
+import config
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+class MyWebdriver:
+    """
+    Класс для управления браузером с использованием Selenium WebDriver.
+    
+    Этот класс позволяет автоматизировать задачи, такие как открытие URL, получение исходного кода страницы и снятие скриншотов.
+    """
